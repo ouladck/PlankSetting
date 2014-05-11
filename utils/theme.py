e31c3a799fd51a4c9f2ccbbfa2c6ab75a4e3e0e2
Karim <herr.linux88@gmail.com>
2014-05-11 12:06:55 +0100

    adding 'add theme' and 'remove theme' buttons

--- utils/theme.py
+++ utils/theme.py
@@ -4,7 +4,8 @@ from gi.repository import Gtk
 from utils.writer import *
 from utils.reader import *
 from gettext import gettext as g
-import os, zipfile
+import os, zipfile, tarfile, shutil
+from utils.rarfile import *
 
 class Theme:
 	def __init__(self, setting_path):
@@ -27,23 +28,94 @@ class Theme:
 		self.read_file()
 		self.select.connect("changed", self.on_tree_selection_changed)
 		
-
+		self.button = Gtk.ButtonBox()
+		self.button.set_spacing(5)
+		self.b = Gtk.FileChooserButton()
+		self.b.set_tooltip_text(g("Add a theme"))
+		self.b.connect('file-set', self.addfunc)
+		self.button.add(self.b)
+		self.b1 = Gtk.Button(stock=Gtk.STOCK_REMOVE)
+		self.b1.set_tooltip_text(g("Remove a theme"))
+		self.b1.connect('clicked', self.delfunc)
+		self.button.add(self.b1)
 		
 		self.scroll.add(self.treeview)
 		
 		self.box = Gtk.VBox()
 		self.box.pack_start(self.scroll, True, True, 15)
+		self.box.pack_start(self.button, False, False, 15)
 		
 	def refresh(self, widget):
+		self.liststore.clear()
 		for a in os.listdir(self.theme_path):
 			widget.append([a])
 	
 	def addfunc(self, widget):
-		pass
+		f = widget.get_filename()
+		bn = os.path.basename(f)
+		name = bn[:bn.find(".")+1]
+		path = self.theme_path + '/' + name
+		newpath = self.theme_path + '/' + name[:name.find('_')].capitalize()
+		if f.find('.') != -1:
+			if f[f.find('.')+1:] == 'rar':
+				try:
+					with RarFile(f, 'r') as fi:
+							if (len(fi.namelist()) == 2) and ('dock.theme' in fi.namelist()) and ('hover.theme' in fi.namelist()):
+								fi.extractall(self.theme_path+'/'+name[:name.find('_')].capitalize(), fi.namelist())
+					os.system("unrar x {0} {1}".format(f, self.theme_path))
+				except:
+					msg = Gtk.MessageDialog(message_type = Gtk.MessageType.WARNING, message_format = g("You dont have a UNRAR program\nPlease make sure to install it before!"))
+					msg.run()
+					msg.destroy()
+			elif f[f.find('.')+1:] == 'tar':
+				with tarfile.TarFile(f, 'r') as fi:
+					if (len(fi.getnames()) == 2) and ('dock.theme' in fi.getnames()) and ('hover.theme' in fi.getnames()):
+						fi.extractall(self.theme_path+'/'+name[:name.find('_')].capitalize(), fi.namelist())
+			elif f[f.find('.')+1:] == 'tar.bz2':
+				with tarfile.open(f, 'r') as fi:
+					i = 0
+					for a in fi.getmembers():
+						if (name+'/'+a.name == 'dock.theme') or (name+'/'+a.name == 'hover.theme'):
+							i += 1
+					fi.extractall(self.theme_path, fi.getmembers())
+			elif f[f.find('.')+1:] == 'tar.gz':
+				with tarfile.open(f, 'r') as fi:
+					i = 0
+					for a in fi.getmembers():
+						if (name+'/'+a.name == 'dock.theme') or (name+'/'+a.name == 'hover.theme'):
+							i += 1
+					fi.extractall(self.theme_path, fi.getmembers())
+			
+			elif f[f.find('.')+1:] == 'zip':
+					try:
+						with zipfile.ZipFile(f, 'r') as fi:
+							if (len(fi.namelist()) == 2) and ('dock.theme' in fi.namelist()) and ('hover.theme' in fi.namelist()):
+								fi.extractall(self.theme_path+'/'+name[:name.find('_')].capitalize(), fi.namelist())
+					except:
+						msg = Gtk.MessageDialog(message_type = Gtk.MessageType.WARNING, message_format = g("You dont have a UNZIP program\nPlease make sure to install it before!"))
+						msg.run()
+						msg.destroy()
+					
+		else:
+			msg = Gtk.MessageDialog(message_type = Gtk.MessageType.WARNING, message_format = g("The theme format is not supported!"))
+			msg.run()
+			msg.destroy()
+		try:
+			os.rename(path, newpath)
+		except:
+			pass
+		self.refresh(self.liststore)
 	
 	def delfunc(self, widget):
-		pass
-	
+		model, treeiter = self.select.get_selected()
+		rmtheme = self.theme_path+'/'+model[treeiter][0]
+		msg = Gtk.MessageDialog(None, 0, Gtk.MessageType.WARNING, Gtk.ButtonsType.OK_CANCEL, g("Do you really wanna remove the theme {} ?").format(model[treeiter][0]))
+		resp = msg.run()
+		if resp == Gtk.ResponseType.OK:
+			shutil.rmtree(rmtheme)
+		msg.destroy()
+		self.refresh(self.liststore)
+
 		
 	def on_tree_selection_changed(self, selection):
 		model, treeiter = selection.get_selected()
